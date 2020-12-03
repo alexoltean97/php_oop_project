069af8b24c3bb55f20e1215bd4731f5b96e8905b
alexoltean97 <alex.oltean97@gmail.com>
2020-12-03 14:29:49 +0200

    Session Class part 1

--- admin/includes/admin_content.php
+++ admin/includes/admin_content.php
@@ -16,31 +16,6 @@
 
             echo $found_user->username;
 
-
-
-//            $users = User::find_all_users();
-//
-//            foreach($users as $user){
-//
-//                echo $user->id;
-//            }
-
-
-
-//            $result_set = User::find_all_users();
-//
-//            while($row = mysqli_fetch_array($result_set)){
-//
-//                echo $row['username'] . "<br>";
-//            }
-//
-
-//            $found_user = User::find_user_by_id(2);
-//
-//            $user = User::instantiation($found_user);
-//
-//            echo $user->id;
-//            echo $user->username;
             ?>
 
             <ol class="breadcrumb">
